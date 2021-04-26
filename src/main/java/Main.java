aba893b6f5d6c4edf6df54f7f99bc782634d161c
Nguyễn Xuân Tùng <nguyenxuantungk37@gmail.com>
2021-04-26 23:30:24 +0700

    update project

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,32 +1,59 @@
 import com.beust.jcommander.Parameter;
+import model.Organization;
+import model.Ticket;
+import model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import picocli.CommandLine.Command;
+import repository.UtilsRepos;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
-public class Main {
+public class Main implements CommandLineRunner {
 
-//    @Command(name = "search")
-//    public void search() {
-//
-//    }
+    @Autowired
+    UtilsRepos repository;
 
-    @Parameter(names={"--length", "-l"})
-    int length;
+    private static final String PATH_USER_FILE = "";
+    private static final String PATH_TICKET_FILE = "";
+    private static final String PATH_ORGANIZATION_FILE = "";
 
-    @Parameter(names={"--pattern", "-p"})
-    int pattern;
+    private static final String INTRODUCTION_MSG = "Type 'quit' to exit at any time. Press 'Enter' to continue" +
+            "\n" +
+            "\tSelect search options:\n" +
+            " \t* Press 1 to search\n" +
+            " \t* Press 2 to view a list of searchable fields\n" +
+            "\t* Type 'quit' to exit\"";
+    private static final String CHOOSE_MODEL = "Select 1) Users or 2) Tickets or 3) Organizations";
+    private static final String SEARCH_TERM = "Enter search term";
+    private static final String SEARCH_VALUE = "Enter search value";
+    private static final String[] USER_SEARCHABLE = {"_id", "url", "external_id", "name", "alias", "created_at", "active",
+            "verified", "shared", "locale", "timezone", "last_login_at", "email", "phone", "signature", "organization_id", "tags", "suspended", "role"};
+    private static final String[] TICKET_SEARCHABLE = {"_id", "url", "external_id", "created_at", "type", "subject", "description",
+            "priority", "status", "submitter_id", "assignee_id", "organization_id", "tags", "has_incidents", "due_at", "via"};
+    private static final String[] ORGANIZATION_SEARCHABLE = {"_id", "url", "external_id", "name", "domain_names", "created_at", "details", "shared_tickets", "tags"};
 
-    public static void search() {
-        Scanner input = new Scanner(System.in);
-        System.out.print("Select 1) Users or 2) Tickets or 3) Organizations: ");
-        int type = input.nextInt();
-        System.out.print("Enter search term: ");
-        String term = input.next().toString();
-        System.out.print("Enter search value: ");
-        String value = input.next().toString();
-    }
+    private String searchTerm = "";
+    private String searchValue = "";
+    private List<Organization> organizations = new ArrayList<Organization>();
+    private List<User> users = new ArrayList<User>();
+    private List<Ticket> tickets = new ArrayList<Ticket>();
 
     public static void main(String[] args) {
         System.out.println("hello world");
     }
+
+    public void run(String... args) throws Exception {
+        // get data from file
+        organizations = repository.readJson(PATH_ORGANIZATION_FILE, Organization.class);
+        users = repository.readJson(PATH_USER_FILE, User.class);
+        tickets = repository.readJson(PATH_TICKET_FILE, Ticket.class);
+
+        // start commanline
+
+    }
+
+
 }
--- src/main/java/model/Ticket.java
+++ src/main/java/model/Ticket.java
@@ -1,4 +1,55 @@
 package model;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class Ticket {
+
+    private static final long serialVersionUID = 1L;
+
+    @JsonAlias("_id")
+    private String id;
+
+    private String url;
+
+    @JsonAlias("external_id")
+    private String externalId;
+
+    @JsonAlias("created_at")
+    private String createdAt;
+
+    private String type;
+
+    private String subject;
+
+    private String description;
+
+    private String priority;
+
+    private String status;
+
+    @JsonAlias("submitter_id")
+    private Integer submitterId;
+
+    @JsonAlias("assignee_id")
+    private Integer assigneeId;
+
+    @JsonAlias("organization_id")
+    private Integer organizationId;
+
+    private String[] tags;
+
+    @JsonAlias("has_incidents")
+    private Boolean hasIncidents;
+
+    @JsonAlias("due_at")
+    private String dueAt;
+
+    private String via;
+
 }
